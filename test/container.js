57116f817da1953d1a7c3f144c3b58e02476a8a2
Dallas Ewing <dallas.ewing@dalane.co.uk>
2016-01-19 15:53:56 +0000

    Added missing files.

--- test/container.js
+++ test/container.js
@@ -1,3 +1,31 @@
-/**
- * Created by dallasewing on 19/01/16.
- */
+var Container = require("../lib/Container");
+
+var assert = require('assert');
+describe('Container', function() {
+    describe('#get()', function () {
+        it('Should throw SyntaxError when the name parameter is not provided.', function () {
+            var error = false;
+            var container = new Container();
+            try {
+                var result = container.get();
+            } catch (err) {
+                if (err.name == 'SyntaxError') {
+                    error = true;
+                }
+            }
+            assert.equal(true, error);
+        });
+        it('Should throw RangeError when the named dependency is not in the collection.', function () {
+            var error = false;
+            var container = new Container();
+            try {
+                var result = container.get('non-existent-dependency');
+            } catch (err) {
+                if (err.name == 'RangeError') {
+                    error = true;
+                }
+            }
+            assert.equal(true, error);
+        });
+    });
+});
