c060a7e59929244ba38392a0ec1e88d0d354b2d5
Dallas Ewing <dallas.ewing@dalane.co.uk>
2016-01-22 14:39:26 +0000

    Tagging completed with 100% code test coverage of new functionality

--- test/container.js
+++ test/container.js
@@ -98,8 +98,8 @@ describe('Container', function() {
             var container = new Container();
             var errors = false;
             try {
-                var tags = ['foo', 'bar'];
-                container.add('passing_tags', function () {}, tags);
+                var tags = ['tag_a', 'tag_b'];
+                container.add('service_with_tags', function () {}, tags);
             } catch (err) {
                 errors = true;
             }
@@ -161,7 +161,7 @@ describe('Container', function() {
         });
     });
     describe('#callable()', function () {
-        it("Should throw a TypeError if attempting to create a callable using a non function.", function () {
+        it("Should throw a TypeError if attempting to create a callable using a non function", function () {
             var container = new Container();
             assert.throws(function () {
                 container.callable('string');
@@ -181,7 +181,7 @@ describe('Container', function() {
         });
     });
     describe('#factory()', function () {
-        it("Should throw a TypeError if attempting to create a factory using a non function.", function () {
+        it("Should throw a TypeError if attempting to create a factory using a non function", function () {
             var container = new Container();
             assert.throws(function () {
                 container.factory('string');
@@ -201,7 +201,7 @@ describe('Container', function() {
         });
     });
     describe('#get()', function () {
-        it('Should throw SyntaxError when the name parameter is not provided.', function () {
+        it('Should throw SyntaxError when the name parameter is not provided', function () {
             var container = new Container();
             assert.throws(function () {
                 container.get();
@@ -209,7 +209,7 @@ describe('Container', function() {
                 return (err.name == 'SyntaxError' && err.message == 'The name parameter is required.');
             });
         });
-        it('Should throw RangeError when the name doesn\'t exist.', function () {
+        it('Should throw RangeError when the name doesn\'t exist', function () {
             var container = new Container();
             // name to test as this appears in the error message
             var name = 'non-existent-name';
@@ -219,7 +219,7 @@ describe('Container', function() {
                 return (err.name == 'RangeError' && err.message == 'The requested dependency "' + name + '" has not been registered. Try checking spelling or correct use of upper and lower case characters.');
             });
         });
-        it('Should return the service object if a service locator function was added.', function () {
+        it('Should return the service object if a service locator function was added', function () {
             var container = new Container();
             container.add('object_factory', function () {
                 return new Object();
@@ -227,17 +227,17 @@ describe('Container', function() {
             var result = container.get('object_factory');
             assert.equal('object', typeof result);
         });
-        it('Should return the string value if a string parameter was added.', function () {
+        it('Should return the string value if a string parameter was added', function () {
             var container = new Container();
             container.add('string', 'string_value');
             assert.equal('string_value', container.get('string'));
         });
-        it('Should return the numeric value if a number parameter was added.', function () {
+        it('Should return the numeric value if a number parameter was added', function () {
             var container = new Container();
             container.add('number', 1);
             assert.equal(1, container.get('number'));
         });
-        it('Should return the object hash if a hash parameter was added.', function () {
+        it('Should return the object hash if a hash parameter was added', function () {
             var container = new Container();
             container.add('hash', {
                 name: 'name'
@@ -245,7 +245,7 @@ describe('Container', function() {
             var result = container.get('hash');
             assert.equal('name', result.name);
         });
-        it('Should return a cached service object rather than invoke the service locator.', function () {
+        it('Should return a cached service object rather than invoke the service locator', function () {
             var container = new Container();
             container.add('mock', function () {
                 return new mock();
@@ -254,7 +254,7 @@ describe('Container', function() {
             var result_two = container.get('mock');
             assert.equal(true, (result_one == result_two));
         });
-        it('Should return the function that has been wrapped by #callable(callable) before being added.', function () {
+        it('Should return the function that has been wrapped by #callable(callable) before being added', function () {
             var container = new Container();
             container.add('callable', container.callable(function () {
                 return '_foo';
@@ -263,7 +263,7 @@ describe('Container', function() {
             assert.equal('function', typeof result);
             assert.equal('_foo', result());
         });
-        it('Should return a new service object if the function has been wrapped by #factory(factory) before being added.', function () {
+        it('Should return a new service object if the function has been wrapped by #factory(factory) before being added', function () {
             var container = new Container();
             var test_function = function () {
                 return new mock();
@@ -274,7 +274,7 @@ describe('Container', function() {
             var result_two = container.get('factory');
             assert.equal(true, (result_one != result_two))
         });
-        it('Should return an object with all of its dependencies automatically populated.', function () {
+        it('Should return an object with all of its dependencies automatically populated', function () {
             var container = new Container();
             container.add('_foo', function () {
                 return new Foo();
@@ -291,8 +291,8 @@ describe('Container', function() {
     describe('#tagged()', function () {
         it("Should return an array of service names tagged by a specific tag", function () {
             var container = new Container();
-            container.add('foo', function () {}, ['tagged', 'foo']);
-            container.add('bar', function () {}, ['tagged', 'bar']);
+            container.add('foo', function () {}, ['tagged', 'tag_a']);
+            container.add('bar', function () {}, ['tagged', 'tag_b']);
             var tagged = container.tagged('tagged');
             assert.equal(true, Array.isArray(tagged));
             assert.equal(true, tagged.length == 2);
@@ -303,9 +303,63 @@ describe('Container', function() {
                 return (value == 'bar');
             }));
         });
+        it("Should throw a TypeError if tag name is not a string", function () {
+            var container = new Container();
+            assert.throws(function () {
+                var tag;
+                container.tagged(tag);
+            }, function (err) {
+                return (err.name == 'TypeError' && err.message == 'tag must be a string.')
+            });
+            assert.throws(function () {
+                var tag = 1;
+                container.tagged(tag);
+            }, function (err) {
+                return (err.name == 'TypeError' && err.message == 'tag must be a string.')
+            });
+            assert.throws(function () {
+                var tag = false;
+                container.tagged(tag);
+            }, function (err) {
+                return (err.name == 'TypeError' && err.message == 'tag must be a string.')
+            });
+            assert.throws(function () {
+                var tag = {};
+                container.tagged(tag);
+            }, function (err) {
+                return (err.name == 'TypeError' && err.message == 'tag must be a string.')
+            });
+            assert.throws(function () {
+                var tag = [];
+                container.tagged(tag);
+            }, function (err) {
+                return (err.name == 'TypeError' && err.message == 'tag must be a string.')
+            });
+            assert.throws(function () {
+                var tag = new Object();
+                container.tagged(tag);
+            }, function (err) {
+                return (err.name == 'TypeError' && err.message == 'tag must be a string.')
+            });
+            assert.throws(function () {
+                var tag = null;
+                container.tagged(tag);
+            }, function (err) {
+                return (err.name == 'TypeError' && err.message == 'tag must be a string.')
+            });
+        });
+        it("Should throw a RangeError if tag name can not be found", function () {
+            var container = new Container();
+            assert.throws(function () {
+                var tag = 'non-existent-tag';
+                container.tagged(tag);
+            }, function (err) {
+                return (err.name == 'RangeError' && err.message == 'tag does not exist.')
+            });
+        });
     });
     describe('#remove()', function () {
-        it('Should throw SyntaxError when the name parameter is not provided.', function () {
+        it('Should throw SyntaxError when the name parameter is not provided', function () {
             var container = new Container();
             assert.throws(function () {
                 container.remove();
@@ -322,7 +376,7 @@ describe('Container', function() {
                 return (err.name == 'RangeError' && err.message == 'The requested dependency "' + non_existent_name + '" has not been registered. Try checking spelling or correct use of upper and lower case characters.');
             });
         });
-        it('Should throw a RangeError when #get() is called after service locator is removed.', function () {
+        it('Should throw a RangeError when #get() is called after service locator is removed', function () {
             var container = new Container();
             var name = 'service-to-remove';
             container.add(name, function () {
@@ -334,6 +388,41 @@ describe('Container', function() {
                 container.remove(name);
             }, function (err) {
                 return (err.name == 'RangeError' && err.message == 'The requested dependency "' + name + '" has not been registered. Try checking spelling or correct use of upper and lower case characters.');
+            });
+        });
+        it('Should remove associated dependencies from the tags when a dependency is removed', function () {
+            var container = new Container();
+            // add two services with tags
+            container.add('foo', function () {}, ['tagged', 'tag_a']);
+            container.add('bar', function () {}, ['tagged', 'tag_b']);
+            // remove the foo service
+            container.remove('foo');
+            // get all the services tagged with "tagged"
+            var tagged = container.tagged('tagged');
+            // there should only be one value (bar) returned as foo was removed
+            assert.equal(true, (tagged.length == 1), "Only one tagged dependency returned");
+            // confirm that foo can not be found
+            assert.equal(false, tagged.some(function (value) {
+                return (value == 'foo');
+            }), "Foo not found");
+            // confirm that bar was returned
+            assert.equal(true, tagged.some(function (value) {
+                return (value == 'bar');
+            }), "Bar found.");
+        });
+        it('Should remove the tag if all associated dependencies have been removed', function () {
+            var container = new Container();
+            // add two services with tags
+            container.add('foo', function () {}, ['tagged', 'tag_a']);
+            container.add('bar', function () {}, ['tagged', 'tag_b']);
+            // remove both the services tagged with 'tagged'
+            container.remove('foo');
+            container.remove('bar');
+            // get all the services tagged with "tagged"
+            assert.throws(function () {
+                var tagged = container.tagged('tagged');
+            }, function (err) {
+                return (err.name == 'RangeError' && err.message == 'tag does not exist.')
             });
         });
     });
