5387f1a476fc55f8d707736c8b903323fec40fb3
Dallas Ewing <dallas.ewing@dalane.co.uk>
2016-09-25 19:57:12 +0100

    changed default factory to always return a new container, added a getSingleton method for those needing a singleton container

--- test/node-di-test.js
+++ test/node-di-test.js
@@ -9,9 +9,14 @@ describe("node-di", function () {
             var result = nodeDi.getContainer();
             expect(result instanceof Container).toBe(true);
         });
-        it("Should always return the same instance of the container", function () {
+        it("Should always return the new instance of the container", function () {
             var result = nodeDi.getContainer();
             var result2 = nodeDi.getContainer();
+            expect((result !== result2)).toBe(true);
+        });
+        it("Should always return the same instance of the container when using getSingleton", function () {
+            var result = nodeDi.getSingleton();
+            var result2 = nodeDi.getSingleton();
             expect((result == result2)).toBe(true);
         });
     });
